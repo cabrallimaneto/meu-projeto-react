996a6842e7d617fc59d741b0f6f283bbd695b4fe
Cabral Lima <cabrallimacont@hotmail.com>
2023-05-27 09:36:05 -0300

    estudando react

---
+++ src/components/MeuComponente.jsx
@@ -0,0 +1,11 @@
+
+export default function MeuComponente() {
+    return (
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio
+            beatae in est eveniet reiciendis accusamus obcaecati, voluptatibus
+            soluta modi aut dolorem, adipisci iure exercitationem veniam totam
+            provident quam animi hic.
+        </p>
+    );
+}
